7a2ec770976b503f0551817b702b7b06ab7934e6
RohitSenta <rohitsenta@gmail.com>
2024-02-28 17:47:24 +0530

    Created React App Using - npm create vite

---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const name = 'Rohit Senta'
+
+  return (
+    <>
+      <h1>Hello World | React With Vite | I am {name}</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 01viteproject/src/main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+function MyApp() {
+	return (
+		<div>
+			<h1>Custom React App !</h1>
+		</div>
+	);
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://google.com",
+//       target: "_blank"
+//   },
+//   children: "Click me to visit google"
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const name = 'Rohit Senta';
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: "_blank"},
+  'Click me to visit google',
+  name
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	// <React.StrictMode>
+		<App /> 
+    // <MyApp />
+    // anotherElement
+    // reactElement
+	// </React.StrictMode>
+);
