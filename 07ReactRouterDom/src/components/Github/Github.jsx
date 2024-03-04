0dc197c7ec98b3afa37b80559663920874675155
RohitSenta <rohitsenta@gmail.com>
2024-03-04 23:53:04 +0530

    React Router Concept - Web Pages with Routes Project

---
+++ 07ReactRouterDom/src/components/Github/Github.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+	// const [data, setData] = useState([]);
+	// useEffect(() => {
+	// 	fetch("https://api.github.com/users/RohitSenta").then((response) =>
+	// 		response.json().then((data) => {
+	// 			console.log("data", data);
+	// 			setData(data);
+	// 		})
+	// 	);
+	// }, []);
+
+    const data = useLoaderData();
+
+	return (
+		<div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+			Github followers: {data.followers}
+			<img
+				src={data.avatar_url}
+				alt="github profile image"
+				width={300}
+				className="mx-auto py-5"
+			/>
+		</div>
+	);
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+	const response = await fetch("https://api.github.com/users/RohitSenta");
+    const data = await response.json();
+    return data;
+};
